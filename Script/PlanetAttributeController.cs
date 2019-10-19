7d800411a415856d169327d4fa5605bfb759f316
Ibrahim Khalil <46520459+bsse1009@users.noreply.github.com>
2019-10-19 17:06:14 +0600

    Update PlanetAttributeController class.
    The change is for more realistic attribute change

--- Script/PlanetAttributeController.cs
+++ Script/PlanetAttributeController.cs
@@ -17,28 +17,32 @@ public class PlanetAttributeController : MonoBehaviour
     public Text VelocityValue;
     public Text DistanceValue;
     public Text TemperatureValue;
-    
+
     public Text DayValue;
     public Text YearValue;
-    
+
 
     public void addGravity()
     {
         float value = float.Parse(GravityValue.text);
         float g1 = value;
-        float g2 = value++;
+        float g2 = value + 1;
+        Math.Round(g2, 1);
         GravityValue.text = g2.ToString();
 
-        float r1 = float.Parse(RadiusValue.text); 
-        double r2 = r1 * (Math.Sqrt(g1/value));
+        float r1 = float.Parse(RadiusValue.text);
+        double r2 = r1 * (Math.Sqrt(g1 / value));
+        Math.Round(r2, 1);
         RadiusValue.text = r2.ToString();
 
         float v1 = float.Parse(VelocityValue.text);
         double v2 = v1 * (r1 / r2);
+        Math.Round(v2, 1);
         VelocityValue.text = v2.ToString();
 
         float d1 = float.Parse(DayValue.text);
         double d2 = d1 * Math.Pow((r2 / r1), 2);
+        Math.Round(d2, 1);
         DayValue.text = d2.ToString();
 
     }
@@ -46,26 +50,31 @@ public class PlanetAttributeController : MonoBehaviour
     {
         float value = float.Parse(GravityValue.text);
         float g1 = value;
-        float g2 = value--;
+        float g2 = value - 1;
+        Math.Round(g2, 1);
         GravityValue.text = g2.ToString();
 
         float r1 = float.Parse(RadiusValue.text);
         double r2 = r1 * (Math.Sqrt(g1 / value));
+        Math.Round(r2, 1);
         RadiusValue.text = r2.ToString();
 
         float v1 = float.Parse(VelocityValue.text);
         double v2 = v1 * (r1 / r2);
+        Math.Round(v2, 1);
         VelocityValue.text = v2.ToString();
 
         float d1 = float.Parse(DayValue.text);
         double d2 = d1 * Math.Pow((r2 / r1), 2);
+        Math.Round(d2, 1);
         DayValue.text = d2.ToString();
     }
 
     public void addMass()
     {
         float value = float.Parse(MassValue.text);
-        value++;
+        value += 0.01;
+        Math.Round(value, 1);
         MassValue.text = value.ToString();
 
         //No noticeable impact
@@ -73,7 +82,8 @@ public class PlanetAttributeController : MonoBehaviour
     public void subtractMass()
     {
         float value = float.Parse(MassValue.text);
-        value--;
+        value -= 0.01;
+        Math.Round(value, 1);
         MassValue.text = value.ToString();
 
         // no noticiable impact
@@ -83,10 +93,12 @@ public class PlanetAttributeController : MonoBehaviour
     {
         float value = float.Parse(RadiusValue.text);
         float r1 = value;
-        float r2 = value++;
+        float r2 = value + 100;
+        Math.Round(r2, 1);
         RadiusValue.text = r2.ToString();
         float g1 = float.Parse(GravityValue.text);
         double g2 = g1 * Math.Pow((r1 / r2), 2);
+        Math.Round(g2, 1);
         GravityValue.text = g2.ToString();
         //m2 = m1;
         //dis2 = dis1;
@@ -94,10 +106,12 @@ public class PlanetAttributeController : MonoBehaviour
 
         float v1 = float.Parse(VelocityValue.text);
         float v2 = v1 * (r1 / r2);
+        Math.Round(v2, 1);
         VelocityValue.text = v2.ToString();
 
         float d1 = float.Parse(DayValue.text);
         double d2 = d1 * Math.Pow((r2 / r1), 2);
+        Math.Round(d2, 1);
         DayValue.text = d2.ToString();
         //y2 = y1;
 
@@ -106,10 +120,12 @@ public class PlanetAttributeController : MonoBehaviour
     {
         float value = float.Parse(RadiusValue.text);
         float r1 = value;
-        float r2 = value--;
+        float r2 = value - 100;
+        Math.Round(r2, 1);
         RadiusValue.text = r2.ToString();
         float g1 = float.Parse(GravityValue.text);
         double g2 = g1 * Math.Pow((r1 / r2), 2);
+        Math.Round(g2, 1);
         GravityValue.text = g2.ToString();
         //m2 = m1;
         //dis2 = dis1;
@@ -117,10 +133,12 @@ public class PlanetAttributeController : MonoBehaviour
 
         float v1 = float.Parse(VelocityValue.text);
         float v2 = v1 * (r1 / r2);
+        Math.Round(v2, 1);
         VelocityValue.text = g2.ToString();
 
         float d1 = float.Parse(DayValue.text);
         double d2 = d1 * Math.Pow((r2 / r1), 2);
+        Math.Round(d2, 1);
         DayValue.text = d2.ToString();
     }
 
@@ -129,15 +147,18 @@ public class PlanetAttributeController : MonoBehaviour
         float value = float.Parse(VelocityValue.text);
         //float value = float.Parse(RadiusValue.text);
         float v1 = value;
-        float v2 = value++;
+        float v2 = value + 2.5;
+        Math.Round(v2, 1);
         VelocityValue.text = v2.ToString();
 
         float r1 = float.Parse(RadiusValue.text);
         float r2 = (v1 / v2) * r1;
+        Math.Round(r2, 1);
         RadiusValue.text = r2.ToString();
 
         float g1 = float.Parse(GravityValue.text);
         double g2 = g1 * Math.Pow((r1 / r2), 2);
+        Math.Round(g2, 1);
         GravityValue.text = g2.ToString();
         //m2 = m1;
         //dis2 = dis1;
@@ -145,6 +166,7 @@ public class PlanetAttributeController : MonoBehaviour
 
         float d1 = float.Parse(DayValue.text);
         double d2 = d1 * Math.Pow((r2 / r1), 2);
+        Math.Round(d2, 1);
         DayValue.text = d2.ToString();
     }
     public void subtractVelocity()
@@ -152,15 +174,18 @@ public class PlanetAttributeController : MonoBehaviour
         float value = float.Parse(VelocityValue.text);
         //float value = float.Parse(RadiusValue.text);
         float v1 = value;
-        float v2 = value--;
+        float v2 = value - 2.5;
+        Math.Round(v2, 1);
         VelocityValue.text = v2.ToString();
 
         float r1 = float.Parse(RadiusValue.text);
         float r2 = (v1 / v2) * r1;
+        Math.Round(r2, 1);
         RadiusValue.text = r2.ToString();
 
         float g1 = float.Parse(GravityValue.text);
         double g2 = g1 * Math.Pow((r1 / r2), 2);
+        Math.Round(g2, 1);
         GravityValue.text = g2.ToString();
         //m2 = m1;
         //dis2 = dis1;
@@ -168,6 +193,7 @@ public class PlanetAttributeController : MonoBehaviour
 
         float d1 = float.Parse(DayValue.text);
         double d2 = d1 * Math.Pow((r2 / r1), 2);
+        Math.Round(d2, 1);
         DayValue.text = d2.ToString();
     }
 
@@ -175,11 +201,13 @@ public class PlanetAttributeController : MonoBehaviour
     {
         float value = float.Parse(DistanceValue.text);
         float dis1 = value;
-        float dis2 = value++;
+        float dis2 = value + 1.0;
+        Math.Round(dis2, 1);
         DistanceValue.text = dis2.ToString();
 
         float t1 = float.Parse(TemperatureValue.text);
         float t2 = (dis2 / dis1) * t1;
+        Math.Round(t2, 1);
         TemperatureValue.text = t2.ToString();
 
     }
@@ -187,11 +215,13 @@ public class PlanetAttributeController : MonoBehaviour
     {
         float value = float.Parse(DistanceValue.text);
         float dis1 = value;
-        float dis2 = value--;
+        float dis2 = value - 1.0;
+        Math.Round(dis2, 1);
         DistanceValue.text = dis2.ToString();
 
         float t1 = float.Parse(TemperatureValue.text);
         float t2 = (dis2 / dis1) * t1;
+        Math.Round(t2, 1);
         TemperatureValue.text = t2.ToString();
     }
 
@@ -199,22 +229,26 @@ public class PlanetAttributeController : MonoBehaviour
     {
         float value = float.Parse(TemperatureValue.text);
         float t1 = value;
-        float t2 = value++;
+        float t2 = value + 5.0;
+        Math.Round(t2, 1);
         DistanceValue.text = t2.ToString();
 
         float dis1 = float.Parse(DistanceValue.text);
-        float dis2 = ( t2/ t1) * dis1;
+        float dis2 = (t2 / t1) * dis1;
+        Math.Round(dis2, 1);
         TemperatureValue.text = dis2.ToString();
     }
     public void subtractTemperature()
     {
         float value = float.Parse(TemperatureValue.text);
         float t1 = value;
-        float t2 = value--;
+        float t2 = value - 5.0;
+        Math.Round(t2, 1);
         DistanceValue.text = t2.ToString();
 
         float dis1 = float.Parse(DistanceValue.text);
         float dis2 = (t2 / t1) * dis1;
+        Math.Round(dis2, 1);
         TemperatureValue.text = dis2.ToString();
     }
 
